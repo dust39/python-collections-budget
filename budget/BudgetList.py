d10d3cfb115fe598065dcd9f65ef25b96dd4ff2e
Dustin Littrell <littrell.dustin@gmail.com>
2021-12-29 01:00:36 -0500

    Module 3 complete, 1st iteration

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,6 @@
 from . import Expense
+
+# Class that extends list type
 class BudgetList():
     def __init__(self, budget):
         self.budget = budget
@@ -7,16 +9,22 @@ class BudgetList():
         self.sum_overages = 0
         self.overages = []
 
+    def __len__(self):
+        return (len(self.expenses) + len(self.overages))
+
+    # implement append so that it only appends to self if total < budget
     def append(self, item):
-        if(self.sum_expenses+item < self.budget):
+        # TODO Check if item is a number
+        if (self.sum_expenses+item < self.budget):
             self.expenses.append(item)
             self.sum_expenses += item
+        # Otherwise append to the overages list and add to the overage total
         else:
             self.overages.append(item)
-            self.sum_overages += item
+            self.sum_overages+=item
 
-    def __len__(self):
-        return len(self.expenses) + len(self.overages)    
+
+
 def main():
     # Using above class
     # Set starting budget to 500
@@ -32,4 +40,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main() 
